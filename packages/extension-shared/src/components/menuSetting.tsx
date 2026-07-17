ff02271b3891f53ba4f55c40242341889f3e7238
Etsi0 <albinkarlsson2003@gmail.com>
2026-07-17 10:37:13 +0200

    increased spacing

--- packages/extension-shared/src/components/menuSetting.tsx
+++ packages/extension-shared/src/components/menuSetting.tsx
@@ -2,7 +2,7 @@ import type { HTMLAttributes } from 'preact';
 import { cn } from '../lib/cn';
 
 const baseClass = `
-	flex justify-between items-center gap-2 w-full px-(--radius-outer) py-[.75em]
+	flex justify-between items-center gap-[calc(.5rem+2pt+2px)] w-full px-(--radius-outer) py-[.75em]
 	rounded-(--radius-controls) first:rounded-t-(--radius-outer) last:rounded-b-(--radius-outer)
 	bg-body-50 dark:bg-body-100
 `;
